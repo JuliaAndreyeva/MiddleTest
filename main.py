bf8c5b81be63709e3660d4d8006df2acfffa4905
JuliaAndreyeva <blackonblackua@gmail.com>
2024-04-05 12:24:33 +0300

    added finding lines with keyword

--- main.py
+++ main.py
@@ -13,10 +13,27 @@ def read_file(input_file: str) -> list[str]:
         return lines
 
 
+def find_lines_with_keyword(lines: list[str], keyword: str) -> list[str]:
+    """
+    Find lines containing a specific keyword within a list of strings.
+
+    Args:
+        lines (list[str]): A list of strings representing lines of text.
+        keyword (str): The keyword to search for within the lines.
+
+    Returns:
+        list[str]: A list of lines from the input list containing the specified keyword.
+    """
+    res = []
+    for line in lines:
+        if keyword in line:
+            res.append(line)
+    return res
+
+
 if __name__ == '__main__':
     try:
         file_data = read_file('data.txt')
+        result = find_lines_with_keyword(file_data, 'Lorem')
     except:
         print('An error occurred')
-
-
